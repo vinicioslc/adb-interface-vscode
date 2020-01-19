3a0541d81e137a4f128c29b024b8995aa7443697
Vinicios Clarindo <vinicioslc@outlook.com>
2020-01-19 11:27:41 -0300

    Removed unused code.

--- src/adb-manager/device-helpers.ts
+++ src/adb-manager/device-helpers.ts
@@ -2,15 +2,6 @@ import { IConsoleInterface } from '../console-interface/iconsole-interface'
 import adbCommands from './adb-commands'
 
 export class DeviceHelpers {
-  static getDeviceManufacturer(
-    ConsoleInstance: IConsoleInterface,
-    ipAddress: string
-  ): string {
-    return ConsoleInstance.execConsoleSync(
-      adbCommands.GET_DEVICE_MODEL(ipAddress)
-    ).toString()
-  }
-
   static getDeviceModel(
     ConsoleInstance: IConsoleInterface,
     deviceIP: string
